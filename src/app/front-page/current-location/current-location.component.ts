c1467f15353677033417f1ef20df3da0fd7cf4f7
DSkiani <D.Skiani@wlv.ac.uk>
2024-02-24 13:25:21 +0200

    current location - latitude and lonitude

--- src/app/front-page/current-location/current-location.component.ts
+++ src/app/front-page/current-location/current-location.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current-location',
   templateUrl: './current-location.component.html',
   styleUrls: ['./current-location.component.scss']
 })
-export class CurrentLocationComponent {
+export class CurrentLocationComponent implements OnInit {
+  latitude: number = 0;
+  longitude: number = 0;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }
 }
