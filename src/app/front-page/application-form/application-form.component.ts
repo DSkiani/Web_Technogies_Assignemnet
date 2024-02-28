f0e9d718d478081bbf3a884046628429dd5f288c
DSkiani <D.Skiani@wlv.ac.uk>
2024-02-29 01:48:50 +0200

    drop-down menu

--- src/app/front-page/application-form/application-form.component.ts
+++ src/app/front-page/application-form/application-form.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-application-form',
   templateUrl: './application-form.component.html',
   styleUrls: ['./application-form.component.scss']
 })
-export class ApplicationFormComponent {
+export class ApplicationFormComponent implements OnInit {
   city: string = '';
+
+  public getJSONValue: any;
+  public cities: any; // Array to hold city names
+
+  constructor(private http: HttpClient) { }
 
   submitForm() {
     console.log('City:', this.city);
     // Here you can perform any action with the form data, like submitting it to a server.
+  }
+
+  ngOnInit(): void {
+    this.getMethod();
+  }
+
+  public getMethod() {
+    this.http.get('http://localhost:8080').subscribe((data: any) => {
+      console.log(data);
+      this.getJSONValue = data;
+      // Extract city names from the JSON data
+      this.cities = this.getJSONValue.weather_project.map((weather: any) => weather.City);
+    });
   }
 }
