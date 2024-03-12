d2be2031692fc73bf4c75adf0a808d24f251a556
DSkiani <D.Skiani@wlv.ac.uk>
2024-03-12 22:42:07 +0200

    test

--- src/app/all-weather/all-weather.component.spec.ts
+++ src/app/all-weather/all-weather.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { AllWeatherComponent } from './all-weather.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 
 describe('AllWeatherComponent', () => {
@@ -10,7 +11,7 @@ describe('AllWeatherComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AllWeatherComponent],
-      imports: [HttpClientModule]
+      imports: [HttpClientModule, HttpClientTestingModule]
     });
     fixture = TestBed.createComponent(AllWeatherComponent);
     component = fixture.componentInstance;
